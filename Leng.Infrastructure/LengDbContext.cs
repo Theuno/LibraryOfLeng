1fed96a982c2b082a28b679c7453d12621f1d976
Theuno <Theuno@users.noreply.github.com>
2023-03-11 19:29:22 +0100

    Add Leng.Infrastructure

---
+++ Leng.Infrastructure/LengDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Leng.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leng.Infrastructure {
+    public class LengDbContext : DbContext {
+        public LengDbContext(DbContextOptions<LengDbContext> options) : base(options) { }
+
+        public DbSet<MTGSets> MTGSets { get; set; }
+        public DbSet<MTGCards> MTGCard { get; set; }
+        public DbSet<LengUser> LengUser { get; set; }
+        public DbSet<LengUserMTGCards> LengUserMTGCards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<LengUser>().HasMany<MTGCards>();
+        }
+    }
+}
