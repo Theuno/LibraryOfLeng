147b41367db636694f07847644106baf3b389e8d
Theuno <Theuno@users.noreply.github.com>
2023-04-12 22:24:11 +0200

    Minimal improvements to pages

--- Leng.BlazorServer/Shared/ShowSheet.cs
+++ Leng.BlazorServer/Shared/ShowSheet.cs
@@ -5,5 +5,9 @@
         public string? setCode { get; set; }
         public int count { get; set; }
         public int countFoil { get; set; }
+        public int want { get; set; }
+        public int wantFoil { get; set; }
+        public string? cardImageUrl { get; set; }
+        public string? setIconUrl { get; set; }
     }
 }
