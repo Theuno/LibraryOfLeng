6bd05c12febb882cbb268b1c3cb76c45c9337258
Theuno <Theuno@users.noreply.github.com>
2023-04-16 19:53:57 +0200

    Add scryfallid

--- Leng.Domain/Models/LengUser.cs
+++ Leng.Domain/Models/LengUser.cs
@@ -1,12 +1,6 @@
-﻿using Leng.Domain.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Reflection.Metadata;
-using System.Security.Policy;
-using System.Text.Json.Serialization;
-using static Leng.Domain.Models.MTGTranslations;
 
 namespace Leng.Domain.Models
 {
